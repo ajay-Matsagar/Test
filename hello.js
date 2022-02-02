bb7495b31ec49fb164fb5180874bd804e2d9038f
ajay-matsagar <ajay.matsagar@thinkitive.com>
2022-02-02 18:54:55 +0530

    Pattern add

--- hello.js
+++ hello.js
@@ -1,3 +1,10 @@
 console.log("Hello Form JAVASCRIPT..")
 console.log("Hello From Master Banch");
 document.write("<h3>Hello Thinkitive</h3>");
+
+        for(let i=0;i<=5;i++){
+            for(let j=0;j<=i;j++){
+                document.write("* ");
+            }
+            document.write("<br>")
+        }
